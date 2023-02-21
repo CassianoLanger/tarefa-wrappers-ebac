0b6bf05d42d781ae267abf13350019e752d5f5af
CassianoLanger <cassianolangerm@gmail.com>
2023-02-21 19:18:16 -0300

    exercicio casting e Wrappers

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author c-lan
+ *
+ */
+module QuartoProjeto {
+}
---
+++ src/program/Main.java
@@ -0,0 +1,25 @@
+package program;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int numl= 15;
+		Integer num = numl;
+		System.out.println(num);
+		
+		byte bi = 1;
+		Byte bite = bi;
+		
+		System.out.println(bite);
+		
+		int inteiro = 123;
+		bite = (byte)inteiro;
+		System.out.println(bite);
+		
+		
+		
+
+	}
+
+}
